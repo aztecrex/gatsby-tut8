ee4ad34611378694cc29d353a40eeb352372fc17
Greg Wiley <aztec.rex@jammm.com>
2019-05-12 14:29:07 -0700

    complete

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,24 +4,40 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 module.exports = {
-    siteMetadata: {
-        title: `Pandas Eating Lots`,
+  siteMetadata: {
+    title: `Pandas Eating Lots`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "GatsbyJS",
+        short_name: "GatsbyJS",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/icon.png", // This path is relative to the root of the site.
+      },
     },
-    plugins: [
-      {
-          resolve: `gatsby-source-filesystem`,
-          options: {
-              name: `src`,
-              path: `${__dirname}/src/`,
-          },
+    'gatsby-plugin-offline',
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
       },
-      `gatsby-transformer-remark`,
-      `gatsby-plugin-emotion`,
-      {
-        resolve: `gatsby-plugin-typography`,
-        options: {
-          pathToConfigModule: `src/utils/typography`,
-        },
+    },
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
       },
-    ],
-  }
+    },
+  ],
+}
